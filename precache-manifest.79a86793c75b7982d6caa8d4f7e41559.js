3398ff7c4bfeaba51e035ae6363e2f071b905b80
Prasanna <prasanna@Prasannas-MacBook-Pro.local>
2019-11-08 19:38:29 +0530

    Updates

---
+++ precache-manifest.79a86793c75b7982d6caa8d4f7e41559.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd239197c4aa1161275cd139f9e416f2",
+    "url": "/Nobel-Laureates/index.html"
+  },
+  {
+    "revision": "80102bd82cb9a25c9143",
+    "url": "/Nobel-Laureates/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "abb2aa70d0d1ca70776c",
+    "url": "/Nobel-Laureates/static/css/main.8dd5a3a7.chunk.css"
+  },
+  {
+    "revision": "80102bd82cb9a25c9143",
+    "url": "/Nobel-Laureates/static/js/2.accb406c.chunk.js"
+  },
+  {
+    "revision": "abb2aa70d0d1ca70776c",
+    "url": "/Nobel-Laureates/static/js/main.6d17c151.chunk.js"
+  },
+  {
+    "revision": "8a6c69493fbba6035453",
+    "url": "/Nobel-Laureates/static/js/runtime-main.fec483d5.js"
+  },
+  {
+    "revision": "03a02e720a402ecc083a4a78541090b5",
+    "url": "/Nobel-Laureates/static/media/NobelPrizeIcon.03a02e72.jpeg"
+  },
+  {
+    "revision": "81c29bc5316124abc2c07a92ebbc23e6",
+    "url": "/Nobel-Laureates/static/media/loading.81c29bc5.gif"
+  }
+]);
